5818f7c5def0f0deb03356a83b7d21ace5572425
Jan Charamza <jan.charamza@rohlik.cz>
2023-06-12 14:06:20 +0200

    add basic styling, vars and images and html structure

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.render('index');
+    const locals = {
+        title: "Nodejs Blog",
+        description: "Simple blog created with Nodejs, Express and MongoDb."
+    }
+
+    res.render('index', locals);
 });
 
 module.exports = router;
