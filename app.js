22c82465a02339be262d1918bb24be68e151f3fd
Jan Charamza <jan.charamza@rohlik.cz>
2023-06-14 14:31:14 +0200

    update mongodb connection to railway service

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
+        mongoUrl: process.env.MONGO_URL
     })
 }))
 
